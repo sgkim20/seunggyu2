881a879f790c02e5d3f3d79feaf3a22f5da64e58
sgkim20 <sgkim20@clvs.co.kr>
2021-09-28 18:08:39 +0900

    Initial commit

---
+++ src/main/java/com/clvs/seunggyu2/Seunggyu2Application.java
@@ -0,0 +1,13 @@
+package com.clvs.seunggyu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seunggyu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seunggyu2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/clvs/seunggyu2/Seunggyu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clvs.seunggyu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seunggyu2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
